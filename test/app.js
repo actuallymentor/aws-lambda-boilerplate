f705f195ca0be9382c7ab6e82670635a8e81eae8
Mentor Palokaj <mentor@palokaj.co>
2019-03-15 21:03:41 +0100

    Better tests

--- test/app.js
+++ test/app.js
@@ -1,21 +1,39 @@
 // Set up chai
 const chai = require( 'chai' )
 const chaiAsPromised = require( 'chai-as-promised' )
-const { expect } = chai
+
+// Config chai
 chai.use( chaiAsPromised )
+chai.should()
 
 // App data etc
 const data = require( `${__dirname}/demodata.json` )
-const { post } = require( `${__dirname}/../app.js` )
+const { post, multiHostPost } = require( `${__dirname}/../app.js` )
 
 describe( 'POSTer validation', f => {
 
 	it( 'Rejects without a host', function() {
-		return expect( post( undefined, '/', {} ) ).to.be.rejected
+		return post( undefined, '/', {} ).should.be.rejected
 	} )
 
 	it( 'Rejects without a url path', function() {
-		return expect( post( 'https://', undefined, {} ) ).to.be.rejected
+		return post( 'https://', undefined, {} ).should.be.rejected
+	} )
+
+} )
+
+describe( 'Multihost POSTer validation', f => {
+
+	it( 'Rejects without a host', function() {
+		return post( undefined, '/', {} ).should.be.rejected
+	} )
+
+	it( 'Rejects without a url path', function() {
+		return post( ['https://'], undefined, {} ).should.be.rejected
+	} )
+
+	it( 'Rejects with a non-array hosts parameter', function() {
+		return post( 'https://', undefined, {} ).should.be.rejected
 	} )
 
 } )
